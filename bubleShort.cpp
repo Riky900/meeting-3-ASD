79dd38fab1b79ee21328d2c010c031d15b3a65f9
Riky900 <rickytripamungkas666@gmail.com>
2025-03-16 21:19:27 +0700

    membuat method display

--- bubleShort.cpp
+++ bubleShort.cpp
@@ -58,6 +58,21 @@ void input()
 
 }
 
+void display() {
+
+    cout << "======================" << endl;
+    cout << "Data yang sudah diurut" << endl;
+    cout << "======================" << endl;
+
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << i+1 << " : " << arr[i] << endl;
+    }
+    
+
+}
+
 int main() {
 
 }
